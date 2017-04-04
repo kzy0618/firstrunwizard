2304c94edeb85709c00326f5c1576aec30db4a72
VoNZproject <voiceofnewzealand@gmail.com>
2017-04-05 10:01:56 +1200

    Customise for VoNZ project

--- templates/wizard.php
+++ templates/wizard.php
@@ -3,64 +3,50 @@
 <a id="closeWizard" class="close">
 	<img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
 </a>
-<h1><?php p($l->t('A safe home for all your data')); ?></h1>
-<?php if ($_['edition'] === \OC_Util::EDITION_COMMUNITY): ?>
-<p><?php p($l->t('Access & share your files, calendars, contacts, mail & more from any device, on your terms'));?></p>
-<?php else: ?>
-<p><?php p($theme->getSlogan()); ?></p>
-<?php endif; ?>
+<h1><?php p($l->t('Welcome to VoNZ Application')); ?></h1>
 
-<h2><?php p($l->t('Get the apps to sync your files'));?></h2>
-<a target="_blank" href="<?php p($_['clients']['desktop']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'desktopapp.svg')); ?>"
-		alt="<?php p($l->t('Desktop client'));?>" />
-</a>
-<a target="_blank" href="<?php p($_['clients']['android']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'googleplay.svg')); ?>"
-		alt="<?php p($l->t('Android app'));?>" style="height:60px"/>
-</a>
-<a target="_blank" href="<?php p($_['clients']['ios']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'appstore.svg')); ?>"
-		alt="<?php p($l->t('iOS app'));?>" style="height:60px"/>
+<p><?php p($l->t('Here you can record some Maori & English languages but first, please read the following rules'));?></p>
+
+<h2><?php p($l->t('Implementation of Background application :')); ?></h2>
+<p><?php p($l->t('To use the database and classify the recording, researchers need some informations about users. Please enter your personnal informations in the background application. This informations will be keep in a secure way and only researchers could access to them.'));?></p>
+
+<h2></h2>
+<a target="_blank">
+	<img src="<?php print_unescaped(OCP\Util::imagePath('firstrunwizard', 'firefoxChrome.jpg')); ?>" style="height:60px"/>
 </a>
 
-<?php if ($_['edition'] === \OC_Util::EDITION_COMMUNITY): ?>
-<h2><?php p($l->t('Connect your desktop apps to %s', array($theme->getName()))); ?></h2>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-calendars')) ?>">
-	<img class="appsmall appsmall-calendar svg" alt=""
-		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/calendar-dark.svg')); ?>" />
-	<?php p($l->t('Connect your Calendar'));?>
+<h2><?php p($l->t('Web Browsers supported :')); ?></h2>
+<p><?php p($l->t('To record your voice, you have to use Mozilla Firefox or Google Chrome. The audio format is not supported by the others web browsers for now.'));?></p>
+
+<h2></h2>
+<a target="_blank">
+	<img src="<?php print_unescaped(OCP\Util::imagePath('firstrunwizard', 'allowMicrophone.png')); ?>" style="height:100px"/>
 </a>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-contacts')) ?>">
-	<img class="appsmall appsmall-contacts svg" alt=""
-		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/contacts-dark.svg')); ?>" />
-	<?php p($l->t('Connect your Contacts'));?>
+
+<h2><?php p($l->t('Utilisation of your microphone :')); ?></h2>
+<p><?php p($l->t('Please give at the application a permanent access to your microphone. If you do not, you could have trouble with the recording duration or to save your data. For this, you can go on your web browsers preferences or click on the left icon of the URL website and change permissions.'));?></p>
+<h2><?php p($l->t('Duration of recording :')); ?></h2>
+<p><?php p($l->t('For each type of recording, you do not have to exceed a maximun duration or your recording will not be save. Here different maximun times for each type of recording : Word = 10 s; List of world (maximun 20) = 30 s; Short phrase = 30 s; Sentence = 1 min; Others type = 1 min'));?></p>
+
+<h2></h2>
+<a target="_blank">
+	<img src="<?php print_unescaped(OCP\Util::imagePath('firstrunwizard', 'smiley.jpg')); ?>" style="height:60px"/>
 </a>
-<?php else: ?>
-<br><br><br>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-manual')); ?>">
-	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('settings', 'help.svg')); ?>" /> <?php p($l->t('Documentation'));?>
-</a>
-<?php endif; ?>
-<a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
-	<img class="appsmall svg" alt=""
-		src="<?php print_unescaped(OCP\Util::imagePath('files', 'folder.svg')); ?>" />
-	<?php p($l->t('Access files via WebDAV'));?>
-</a>
+
+<h2><?php p($l->t('Download your recording :')); ?></h2>
+<p><?php p($l->t('At the end of your recording you can download the files on the server to contribute to our project, add your recording to the database and share with others users and researchers.'));?></p>
 
 <p class="footnote">
-<?php if ($_['edition'] === ''): ?>
-<?php print_unescaped($l->t('There’s more information in the <a target="_blank" href="%s">documentation</a> and on our <a target="_blank" href="http://owncloud.org">website</a>.', array(link_to_docs('user_manual')))); ?><br>
-<?php print_unescaped($l->t('If you like ownCloud,
+<?php print_unescaped($l->t('There’s more information in the <a target="_blank" href="https://github.com/VoNZproject">documentation</a>')); ?><br>
+<?php print_unescaped($l->t('If you like VoNZ,
 	<a href="mailto:?subject=ownCloud
-		&body=ownCloud is a great open software to sync and share your files.
-		You can freely get it from http://owncloud.org">
-		recommend it to your friends</a>
-	and <a href="http://owncloud.org/promote"
-		target="_blank">spread the word</a>!')); ?>
-<?php else: ?>
-© <?php p(date('Y')) ?> <a href="<?php p($theme->getBaseUrl()); ?>" target="_blank"><?php p($theme->getEntity()); ?></a>
-<?php endif; ?>
+		&body=VoNZ is a great application to deal with maori language and differents New Zealand English accents. Join our community ! 
+		You can freely get it by register from https://130.216.118.226">
+		recommend it to your friends</a>!')); ?>
+
 </p>
+<a class="box">
+	<input id="disablerun" type="checkbox"> Don't show me this windows again !
+</a>
 
 </div>
