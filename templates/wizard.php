cd2e19a73f6efaaf799ab425fdcbb58ade3fe6eb
VoNZproject <voiceofnewzealand@gmail.com>
2017-05-18 16:56:33 +1200

    Update wizard.php

--- templates/wizard.php
+++ templates/wizard.php
@@ -3,12 +3,9 @@
 <a id="closeWizard" class="close">
 	<img class="svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'actions/close.svg')); ?>">
 </a>
-<h1><?php p($l->t('Welcome to VoNZ Application')); ?></h1>
+<h1><?php p($l->t('Welcome to VoNZ Project')); ?></h1>
 
 <p><?php p($l->t('Here you can record some Maori & English languages but first, please read the following rules'));?></p>
-
-<h2><?php p($l->t('Add your personnal informations :')); ?></h2>
-<p><?php p($l->t('To use the database and classify the recording, researchers need some informations about users. Please enter your personnal informations in the BACKGROUND application. This informations will be keep in a secure way and only researchers could access to them.'));?></p>
 
 <h2></h2>
 <a target="_blank">
@@ -20,13 +17,19 @@
 
 <h2></h2>
 <a target="_blank">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('firstrunwizard', 'allowMicrophone.png')); ?>" style="height:100px"/>
+	<img src="<?php print_unescaped(OCP\Util::imagePath('firstrunwizard', 'background.jpg')); ?>" style="height:60px"/>
+</a>
+	
+<h2><?php p($l->t('Add your personnal informations :')); ?></h2>
+<p><?php p($l->t('To use the database and classify the recording, researchers need some informations about users. Please enter your personnal informations in the BACKGROUND application. This informations will be keep in a secure way and only researchers could access to them.'));?></p>
+
+<h2></h2>
+<a target="_blank">
+        <img src="<?php print_unescaped(OCP\Util::imagePath('firstrunwizard', 'recorder.png')); ?>" style="height:60px"/>
 </a>
 
-<h2><?php p($l->t('Utilisation of your microphone :')); ?></h2>
-<p><?php p($l->t('Please give at the application a permanent access to your microphone. If you do not, you could have trouble with the recording duration or to save your data. For this, you can go on your web browsers preferences or click on the left icon of the URL website and change permissions.'));?></p>
-<h2><?php p($l->t('Duration of recording :')); ?></h2>
-<p><?php p($l->t('For each type of recording, you do not have to exceed a maximun duration or your recording will not be save. Here different maximun times for each type of recording : Word = 10 s; List of world (maximun 20) = 30 s; Short phrase = 30 s; Sentence = 1 min; Others type = 1 min'));?></p>
+<h2><?php p($l->t('Record yourself :')); ?></h2>
+<p><?php p($l->t('Go to the recorder application and record different types of data about Maori and English languages.'));?></p>
 
 <h2></h2>
 <a target="_blank">
